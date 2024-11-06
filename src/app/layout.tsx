f01d0eeb515818000355ff59d0c76bdb5a7fa0fb
scseong <goidtv@naver.com>
2024-11-06 14:57:02 +0900

    feat: supabase 연동하여 홈 배너 API 호출 테스트 - supabase 연동 - supabase home_banner 테이블 생성 - API 호출 후 응답 확인하여 화면에 표시

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,31 @@
-import type { Metadata } from "next";
-import localFont from "next/font/local";
-import "./globals.css";
+import type { Metadata } from 'next';
+import localFont from 'next/font/local';
+import './globals.css';
 
 const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900'
 });
 const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900'
 });
 
 export const metadata: Metadata = {
-  title: "동남교회",
-  description: "서로서로 세워가는 교회, 동남교회",
+  title: '동남교회',
+  description: '서로서로 세워가는 교회, 동남교회'
 };
 
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html lang="ko">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,25 @@
-export default function Home() {
-  return <div>Home</div>;
+/* eslint-disable @next/next/no-img-element */
+import { getHomeBanner } from '@/apis/home';
+
+export default async function Home() {
+  const home_banner = await getHomeBanner();
+  const { id, title, description, image_url, order, year } = home_banner[0];
+
+  return (
+    <div className="banner">
+      <img src={image_url} alt={title} className="banner-image" />
+      <div className="banner-content">
+        <h3 className="banner-title">{title}</h3>
+        <p className="banner-description">
+          {description &&
+            description.split('\n').map((line, index) => (
+              <span key={index}>
+                {line}
+                <br />
+              </span>
+            ))}
+        </p>
+      </div>
+    </div>
+  );
 }
