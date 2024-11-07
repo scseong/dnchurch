1cf9bd661dfa7eb4fa8b114085006dffb056c810
scseong <goidtv@naver.com>
2024-11-07 14:15:57 +0900

    Feat: 커스텀 폰트 NotoSansKR 적용 - Google Fonts에서 다운받아 ttf to woff2 - NextJS의 Local Fonts 문서 참조 - https://nextjs.org/docs/app/building-your-application/optimizing/fonts#local-fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import Header from './_component/Header';
 import Footer from './_component/Footer';
+import localFont from 'next/font/local';
 import type { Metadata } from 'next';
 import './globals.css';
 
@@ -8,13 +9,44 @@ export const metadata: Metadata = {
   description: '서로서로 세워가는 교회, 동남교회'
 };
 
+const myFont = localFont({
+  src: [
+    {
+      path: './fonts/NotoSansKR-Light.woff2',
+      weight: '300',
+      style: 'normal'
+    },
+    {
+      path: './fonts/NotoSansKR-Regular.woff2',
+      weight: '400',
+      style: 'normal'
+    },
+    {
+      path: './fonts/NotoSansKR-Medium.woff2',
+      weight: '500',
+      style: 'normal'
+    },
+    {
+      path: './fonts/NotoSansKR-SemiBold.woff2',
+      weight: '600',
+      style: 'normal'
+    },
+    {
+      path: './fonts/NotoSansKR-Bold.woff2',
+      weight: '700',
+      style: 'normal'
+    }
+  ],
+  variable: '--font-notosans'
+});
+
 export default function RootLayout({
   children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={myFont.variable}>
       <body>
         <div id="root">
           <Header />
