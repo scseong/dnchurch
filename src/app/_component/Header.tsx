227c9479545c688285b4adc815da883a6d959d42
scseong <goidtv@naver.com>
2024-11-12 18:30:29 +0900

    Feat: 헤더 반응형 적용 - 모바일 버전에서 햄버거 버튼 활용하여 메뉴 토글 - mixin 사용하여 반응형 웹 - 모바일 우선 고려

--- src/app/_component/Header.tsx
+++ src/app/_component/Header.tsx
@@ -1,7 +1,36 @@
+'use client';
+
+import { useState, useEffect, useRef } from 'react';
 import Link from 'next/link';
 import styles from './Header.module.scss';
+import { GiHamburgerMenu } from 'react-icons/gi';
 
 export default function Header() {
+  const [isNavVisible, setNavVisible] = useState(false);
+  const navRef = useRef<HTMLDivElement>(null);
+
+  const toggleNav = () => {
+    setNavVisible((prev) => !prev);
+  };
+
+  const handleClickOutside = (event: MouseEvent | PointerEvent) => {
+    if (navRef.current && !navRef.current.contains(event.target as Node)) {
+      setNavVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    if (isNavVisible) {
+      document.addEventListener('click', handleClickOutside);
+    } else {
+      document.removeEventListener('click', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, [isNavVisible]);
+
   return (
     <header className={styles.header}>
       <div className={styles.header_wrap}>
@@ -13,32 +42,42 @@ export default function Header() {
             </Link>
           </h1>
         </div>
-        <nav className={styles.nav}>
-          <ul>
-            <li>
-              <Link href="/about">교회소개</Link>
-            </li>
-            <li>
-              <Link href="/news">교회소식</Link>
-            </li>
-            <li>
-              <Link href="/fellowship">교제</Link>
-            </li>
-            <li>
-              <Link href="/gallery">동남앨범</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className={styles.auth}>
-          <ul>
-            <li>
-              {/* TODO: 모달로 구현 */}
-              <Link href="/login">로그인</Link>
-            </li>
-            <li>회원가입</li>
-          </ul>
+        {isNavVisible && (
+          <>
+            <nav className={styles.nav} ref={navRef}>
+              <ul>
+                <li>
+                  <Link href="/about">교회소개</Link>
+                </li>
+                <li>
+                  <Link href="/news">교회소식</Link>
+                </li>
+                <li>
+                  <Link href="/fellowship">교제</Link>
+                </li>
+                <li>
+                  <Link href="/gallery">동남앨범</Link>
+                </li>
+              </ul>
+            </nav>
+            <div className={styles.auth}>
+              <ul>
+                <li>
+                  {/* TODO: 모달로 구현 */}
+                  <Link href="/login">로그인</Link>
+                </li>
+                <li>회원가입</li>
+              </ul>
+            </div>
+          </>
+        )}
+        <div className={styles.toggle}>
+          <button onClick={toggleNav} aria-label="Toggle Navigation">
+            <GiHamburgerMenu size="1rem" />
+          </button>
         </div>
       </div>
+      {isNavVisible && <div className={styles.overlay} />}
     </header>
   );
 }
