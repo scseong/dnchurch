d3d05cbcc65efa63f0a72bd5cdd56d5535b1bc50
scseong <goidtv@naver.com>
2024-11-19 16:11:55 +0900

    Feat: 메인 페이지 환영인사글 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Banner from './_component/Banner';
+import Greeting from './_component/Greeting';
 
 export default async function Home() {
-  return <Banner />;
+  return (
+    <>
+      <Banner />
+      <Greeting />
+    </>
+  );
 }
