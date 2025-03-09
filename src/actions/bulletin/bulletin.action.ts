82092b08ef8387460e362f7d573c049c8f80a89c
scseong <goidtv@naver.com>
2025-03-10 04:25:24 +0900

    Feat: 주보 Storage와 Table 수정 로직

--- src/actions/bulletin/bulletin.action.ts
+++ src/actions/bulletin/bulletin.action.ts
@@ -1,9 +1,9 @@
 'use server';
 
 import { redirect, RedirectType } from 'next/navigation';
-import { revalidatePath } from 'next/cache';
+import { revalidatePath, revalidateTag } from 'next/cache';
 import { createServerSideClient } from '@/shared/supabase/server';
-import { uploadFileAction } from '../file.action';
+import { updateFileAction, uploadFileAction } from '../file.action';
 import { getUrlsFromApiResponse } from '@/shared/util/file';
 import type { ImageFileData } from '@/shared/types/types';
 
@@ -51,4 +51,63 @@ export const createBulletinAction = async (
   revalidatePath('/news');
   revalidatePath('/news/bulletin');
   redirect('/news/bulletin', RedirectType.push);
+};
+
+export const updateBulletinAction = async (
+  selectedFiles: ImageFileData[],
+  state: { status: boolean; payload?: FormData; error?: string } | null | undefined,
+  formData: FormData
+) => {
+  const bulletinId = formData.get('bulletin_id')?.toString();
+  const title = formData.get('title')?.toString().trim();
+  const isDisabled = formData.get('is_disabled');
+  let imagefileUrls = [] as string[];
+  const updatedObject = {} as UpdateObject;
+
+  if (isDisabled) {
+    return {
+      status: false,
+      error: '변경된 내역이 없습니다.'
+    };
+  }
+
+  if (selectedFiles.length > 0) {
+    const uploadPromises = selectedFiles.map((file) => updateFileAction(file));
+    const uploadResults = await Promise.all(uploadPromises);
+    imagefileUrls = getUrlsFromApiResponse(uploadResults);
+
+    updatedObject.image_url = imagefileUrls;
+  }
+
+  if (title) {
+    updatedObject.title = title;
+  }
+
+  try {
+    const supabase = await createServerSideClient({});
+    const { error } = await supabase
+      .from('bulletin')
+      .update({ ...updatedObject })
+      .eq('id', Number(bulletinId))
+      .select();
+
+    if (error) {
+      throw new Error(error.message);
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      status: false,
+      error: `주보 생성에 실패했습니다: ${error}`
+    };
+  }
+
+  revalidateTag('bulletin');
+  revalidatePath('/news/bulletin');
+  redirect(`/news/bulletin/${bulletinId}`, RedirectType.push);
+};
+
+type UpdateObject = {
+  title?: string;
+  image_url?: string[];
 };
--- src/actions/file.action.ts
+++ src/actions/file.action.ts
@@ -35,3 +35,35 @@ export const uploadFileAction = async (file: ImageFileData) => {
     };
   }
 };
+
+export const updateFileAction = async (file: ImageFileData) => {
+  const filename = extractNumbersFromString(file.filename);
+  const fileimage = file.fileimage as string;
+  const base64 = fileimage.split('base64,')[1];
+  const buffer = decode(base64);
+
+  try {
+    const supabase = await createServerSideClient({});
+    const { data, error } = await supabase.storage.from('bulletin').update(filename, buffer, {
+      contentType: file.filetype,
+      cacheControl: '3600'
+    });
+
+    if (error) {
+      console.error(error);
+    }
+
+    return {
+      status: true,
+      data: {
+        url: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/storage/v1/object/public/${data?.fullPath}`
+      },
+      error: ''
+    };
+  } catch (error) {
+    return {
+      status: false,
+      error: `파일 업로드에 실패했습니다: ${error}`
+    };
+  }
+};
