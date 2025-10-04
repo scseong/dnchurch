d90d9d30536c4ecc7f38c494fd99610b631010b8
scseong <goidtv@naver.com>
2025-10-05 01:52:14 +0900

    refactor: 헤더 디자인 개선

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import { Banner, Greeting, HomeGallery, LinkButtons } from './_component/home';
 export default async function Home() {
   return (
     <>
-      <Banner />
+      {/* <Banner /> */}
       <Greeting />
       <LinkButtons />
       <HomeGallery />
