51d67b4406ca4f37c3992d3d22989d2caf7d6e95
scseong <goidtv@naver.com>
2025-10-13 23:57:13 +0900

    Feat: 배너 Mobile First 반응형 적용

--- src/app/_component/home/Banner.tsx
+++ src/app/_component/home/Banner.tsx
@@ -1,16 +1,28 @@
+import Link from 'next/link';
 import LayoutContainer from '../layout/common/LayoutContainer';
 import styles from './Banner.module.scss';
 
 export default async function Banner() {
   return (
-    <section className={styles.section}>
+    <section>
       <div className={styles.banner}>
         <LayoutContainer>
-          <h2>
-            동남교회에 오신 것을
-            <br />
-            환영합니다
-          </h2>
+          <div className={styles.content}>
+            <h2>
+              동남교회에 오신 것을
+              <br />
+              환영합니다
+            </h2>
+            <p>
+              수고하고 무거운 짐진 당신을 주님의 사랑으로 초대합니다.
+              <br />
+              마음의 짐을 내려놓고 참된 안식을 누리세요.
+            </p>
+
+            <Link href="/about" className={styles.learn_more}>
+              자세히 보기
+            </Link>
+          </div>
         </LayoutContainer>
       </div>
     </section>
