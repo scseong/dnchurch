a1fc82776f9fa42a856abcef4486efb85334e421
scseong <goidtv@naver.com>
2025-10-15 21:31:31 +0900

    Fix: 초기 렌더링 네비게이션 깜빡임(FOUC) 해결 및 CSS 반응형 전환

--- src/app/_component/layout/header/DesktopNav.tsx
+++ src/app/_component/layout/header/DesktopNav.tsx
@@ -2,13 +2,9 @@ import Link from 'next/link';
 import { sitemap } from '@/shared/constants/sitemap';
 import styles from './index.module.scss';
 
-type Props = {
-  isVisible: boolean;
-};
-
-export default function DesktopNav({ isVisible }: Props) {
+export default function DesktopNav() {
   return (
-    <nav className={`${styles.nav} ${isVisible ? styles.visible : styles.invisible}`}>
+    <nav className={styles.desktop_nav}>
       <ul>
         {sitemap
           .filter((item) => item.show)
