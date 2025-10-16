cf5a9f2805e62deb157cc1584609f29cde9b40d9
scseong <goidtv@naver.com>
2025-10-16 23:08:47 +0900

    Chore: 로고에 헤딩 태그를 삭제하고 메인 텍스트에 h1 태그 적용

--- src/app/_component/home/Banner.tsx
+++ src/app/_component/home/Banner.tsx
@@ -8,17 +8,16 @@ export default async function Banner() {
       <div className={styles.banner}>
         <LayoutContainer>
           <div className={styles.content}>
-            <h2>
+            <h1>
               동남교회에 오신 것을
               <br />
               환영합니다
-            </h2>
+            </h1>
             <p>
               수고하고 무거운 짐진 당신을 주님의 사랑으로 초대합니다.
               <br />
               마음의 짐을 내려놓고 참된 안식을 누리세요.
             </p>
-
             <Link href="/about" className={styles.learn_more}>
               자세히 보기
             </Link>
--- src/app/_component/layout/header/Logo.tsx
+++ src/app/_component/layout/header/Logo.tsx
@@ -4,9 +4,7 @@ import styles from './index.module.scss';
 export default function Logo() {
   return (
     <div className={styles.logo}>
-      <h1>
-        <Link href="/">대구동남교회</Link>
-      </h1>
+      <Link href="/">대구동남교회</Link>
     </div>
   );
 }
