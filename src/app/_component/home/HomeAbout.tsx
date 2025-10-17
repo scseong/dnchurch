05e3ff5fa3ef2b226dd6cbd4e2ce2bf060b966c8
scseong <goidtv@naver.com>
2025-10-17 23:57:29 +0900

    Feat: 메인 페이지 교회 설명 기본 UI 구현

--- src/app/_component/home/HomeAbout.tsx
+++ src/app/_component/home/HomeAbout.tsx
@@ -1,19 +1,49 @@
+import { PiChurch } from 'react-icons/pi';
+import { LayoutContainer } from '../layout/common';
+import IconWrap from '../common/IconWrap';
 import styles from './HomeAbout.module.scss';
 
 export default function HomeAbout() {
   return (
     <section className={styles.about}>
-      <div className={styles.container}>
-        <div className={styles.content_wrap}>
-          <div className={styles.image}>
-            <img src="/images/church2.png" alt="동남교회" />
+      <LayoutContainer>
+        <div className={styles.title}>
+          <span className={styles.caption}>우리의 비전</span>
+          <h2>동남교회의 핵심 사역</h2>
+        </div>
+        <div className={styles.feature_container}>
+          <div className={styles.feature}>
+            <div className={styles.icon}>
+              <IconWrap Icon={PiChurch} />
+            </div>
+            <h5>말씀과 훈련</h5>
+            <p>
+              성경공부, 제자훈련, 신앙교육을 통해 하나님의 주권을 높이는 견고한 개혁주의 신앙 위에
+              서도록 훈련합니다.
+            </p>
           </div>
-          <div>
-            <h3>동남교회는</h3>
-            <p>...</p>
+          <div className={styles.feature}>
+            <div className={styles.icon}>
+              <IconWrap Icon={PiChurch} />
+            </div>
+            <h5>말씀과 훈련</h5>
+            <p>
+              성경공부, 제자훈련, 신앙교육을 통해 하나님의 주권을 높이는 견고한 개혁주의 신앙 위에
+              서도록 훈련합니다.
+            </p>
+          </div>
+          <div className={styles.feature}>
+            <div className={styles.icon}>
+              <IconWrap Icon={PiChurch} />
+            </div>
+            <h5>말씀과 훈련</h5>
+            <p>
+              성경공부, 제자훈련, 신앙교육을 통해 하나님의 주권을 높이는 견고한 개혁주의 신앙 위에
+              서도록 훈련합니다.
+            </p>
           </div>
         </div>
-      </div>
+      </LayoutContainer>
     </section>
   );
 }
