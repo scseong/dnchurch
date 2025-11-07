3bb70e43e922fdc0bd9e0ea81b98f279160a4522
scseong <goidtv@naver.com>
2025-11-07 17:58:04 +0900

    Chore: 불필요한 파일 및 중복 코드 삭제

--- src/app/_component/home/index.ts
+++ src/app/_component/home/index.ts
@@ -1,8 +1,6 @@
 export { default as Banner } from './Banner';
 export { default as AboutOurChurch } from './AboutOurChurch';
-export { default as HomeHero } from './HomeHero';
 export { default as ChurchVision } from './ChurchVision';
 export { default as HomeGallery } from './HomeGallery';
 export { default as GalleryCarousel } from './HomeGalleryCarousel';
-export { default as Greeting } from './Greeting';
 export { default as LinkButtons } from './LinkButtons';
